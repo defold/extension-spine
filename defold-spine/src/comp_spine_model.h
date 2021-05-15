576b7298ecb1cfa372f4e9ea540a092707d4320d
JCash <mwesterdahl76@gmail.com>
2021-05-15 09:16:24 +0200

    Working resource/comonent parts

---
+++ defold-spine/src/comp_spine_model.h
@@ -0,0 +1,64 @@
+// Copyright 2020 The Defold Foundation
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+//
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#ifndef DM_GAMESYS_COMP_SPINE_MODEL_H
+#define DM_GAMESYS_COMP_SPINE_MODEL_H
+
+#include <stdint.h>
+#include <dmsdk/dlib/array.h>
+#include <dmsdk/dlib/hash.h>
+#include <dmsdk/dlib/vmath.h>
+#include <dmsdk/gameobject/gameobject.h>
+#include <dmsdk/gamesys/render_constants.h>
+#include <dlib/object_pool.h>
+//#include <graphics/graphics.h>
+#include <dmsdk/resource/resource.h>
+#include <dmsdk/render/render.h>
+#include <dmsdk/rig/rig.h>
+//#include "comp_private.h"
+
+#include "res_spine_model.h"
+
+namespace dmSpine
+{
+    struct SpineModelComponent
+    {
+        dmGameObject::HInstance                 m_Instance;
+        dmTransform::Transform                  m_Transform;
+        dmVMath::Matrix4                        m_World;
+        SpineModelResource*                     m_Resource;
+        dmRig::HRigInstance                     m_RigInstance;
+        dmMessage::URL                          m_Listener;
+        dmGameSystem::HComponentRenderConstants m_RenderConstants;
+        dmRender::HMaterial                     m_Material;
+        /// Node instances corresponding to the bones
+        dmArray<dmGameObject::HInstance>        m_NodeInstances;
+        uint32_t                                m_MixedHash;
+        uint16_t                                m_ComponentIndex;
+        /// Component enablement
+        uint8_t                                 m_Enabled : 1;
+        uint8_t                                 m_DoRender : 1;
+        /// Added to update or not
+        uint8_t                                 m_AddedToUpdate : 1;
+        uint8_t                                 m_ReHash : 1;
+    };
+
+    // For scripting
+    bool CompSpineModelSetIKTargetInstance(SpineModelComponent* component, dmhash_t constraint_id, float mix, dmhash_t instance_id);
+    bool CompSpineModelSetIKTargetPosition(SpineModelComponent* component, dmhash_t constraint_id, float mix, Vectormath::Aos::Point3 position);
+    bool CompSpineModelResetIKTarget(SpineModelComponent* component, dmhash_t constraint_id);
+
+    bool CompSpineModelSetSkin(SpineModelComponent* component, dmhash_t skin_id);
+    bool CompSpineModelSetSkinSlot(SpineModelComponent* component, dmhash_t skin_id, dmhash_t slot_id);
+}
+
+#endif // DM_GAMESYS_COMP_SPINE_MODEL_H
--- defold-spine/src/extension.cpp
+++ defold-spine/src/extension.cpp
@@ -1,2 +1,29 @@
-extern "C" void SpineExt() {}
 
+#include <dmsdk/sdk.h>
+#include "script_spine_model.h"
+
+static dmExtension::Result AppInitializeSpine(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeSpine(dmExtension::Params* params)
+{
+    dmSpine::ScriptSpineModelRegister(params->m_L);
+    dmLogInfo("Registered spine extension\n");
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeSpine(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeSpine(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+
+// DM_DECLARE_EXTENSION(symbol, name, app_init, app_final, init, update, on_event, final)
+DM_DECLARE_EXTENSION(SpineExt, "SpineExt", AppInitializeSpine, AppFinalizeSpine, InitializeSpine, 0, 0, FinalizeSpine);
