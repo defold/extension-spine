5231e87ac66e82bedcc81593c4cf4fe0d66d1a07
JCash <mwesterdahl76@gmail.com>
2021-10-08 10:20:40 +0200

    removed debug output

--- defold-spine/pluginsrc/com/defold/bob/pipeline/SpineModelBuilder.java
+++ defold-spine/pluginsrc/com/defold/bob/pipeline/SpineModelBuilder.java
@@ -37,8 +37,6 @@ public class SpineModelBuilder extends ProtoBuilder<SpineModelDesc.Builder> {
             BuilderUtil.checkResource(this.project, resource, "material", messageBuilder.getMaterial());
         }
         messageBuilder.setMaterial(BuilderUtil.replaceExt(messageBuilder.getMaterial(), ".material", ".materialc"));
-
-        System.out.printf("MAWE: OUTPUT FROM EXTENSION SpineModelBuilder!\n");
         return messageBuilder;
     }
 }
