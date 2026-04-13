dd32fde5becf4e2a18db275d17bf67bacd653eb0
AGulev <me@agulev.com>
2026-04-13 12:37:53 +0200

    add basic perf. test

---
+++ examples/performance_test/performance_spine.go
@@ -0,0 +1,13 @@
+embedded_components {
+  id: "spinemodel"
+  type: "spinemodel"
+  data: "spine_scene: \"/assets/spineboy/spineboy.spinescene\"\n"
+  "default_animation: \"jump\"\n"
+  "skin: \"\"\n"
+  "material: \"/defold-spine/assets/spine.material\"\n"
+  ""
+  scale {
+    x: 0.08
+    y: 0.08
+  }
+}
